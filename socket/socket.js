7fb5ce38257ac711cd9388cfb7421139cae67b31
Zach Serafini <serafini.8@osu.edu>
2014-10-29 20:24:12 -0400

    kill overuse of 'socket' word

--- socket/socket.js
+++ socket/socket.js
@@ -1,39 +1,39 @@
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var map = require('../lib/map');
-socket_helpers = require('../lib/socket-helpers');
+user = require('../lib/user');
 _ = require('lodash-node');
 map.initialize();
 
-io.on('connection', function(socket){
+io.on('connection', function(socket_controller){
 
   console.log('a user connected');
 
-  socket.on('map request', function(coordinates){
-    socket.emit('map request', map.get_local_map(socket, coordinates));
+  socket_controller.on('map request', function(coordinates){
+    socket_controller.emit('map request', map.get_local_map(socket_controller, coordinates));
   });
 
-  socket.on('update player position', function(player_data){
+  socket_controller.on('update player position', function(player_data){
     var cleaned_player = _.extend({type: 'Other Player'}, player_data);
     var old_player_data = map.get_player(player_data.username);
-    var updated_map = map.update_local_map(socket, old_player_data, player_data);
-    var current_room = socket_helpers.get_socket_room_name(cleaned_player);
-    socket.emit('update map', updated_map);
+    var updated_map = map.update_local_map(socket_controller, old_player_data, player_data);
+    var current_room = user.get_room_name(cleaned_player);
+    socket_controller.emit('update map', updated_map);
     map.update_player(cleaned_player);
-    socket.broadcast.to(current_room).emit('update player position', cleaned_player);
+    socket_controller.broadcast.to(current_room).emit('update player position', cleaned_player);
   });
 
-  socket.on('player request', function(player_data){
+  socket_controller.on('player request', function(player_data){
     var uniqueID = Math.random().toString(); 
     var new_player = {username: player_data.username, coordinate: {x: 130, y: 130, z: 1}, uniqueID: uniqueID};
     var cleaned_player = _.extend({type: 'Other Player'}, new_player);
-    var current_room = socket_helpers.get_socket_room_name(cleaned_player);
+    var current_room = user.get_room_name(cleaned_player);
     map.add_player(cleaned_player);
-    socket.emit('player request', new_player);
-    socket.broadcast.to(current_room).emit('add other player', cleaned_player);
+    socket_controller.emit('player request', new_player);
+    socket_controller.broadcast.to(current_room).emit('add other player', cleaned_player);
   });
 
-  socket.on('disconnect', function(){
+  socket_controller.on('disconnect', function(){
     console.log('user disconnected');
   });
 
