7599a41607ff914059fd1cab042f7f64b2133417
Zach Serafini <serafini.8@osu.edu>
2014-10-29 22:49:30 -0400

    Pull out user model

--- socket/socket.js
+++ socket/socket.js
@@ -1,23 +1,26 @@
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var map = require('../lib/map');
+var user = require('../lib/user');
 socket_helpers = require('../lib/socket-helpers');
+map = require('../lib/map');
 _ = require('lodash-node');
 map.initialize();
 
 io.on('connection', function(socket){
 
   console.log('a user connected');
+  var current_user = user.new_user(socket);
 
   socket.on('map request', function(coordinates){
-    socket.emit('map request', map.get_local_map(socket, coordinates));
+    full_map = current_user.get_map(coordinates);
+    socket.emit('map request', full_map);
   });
 
   socket.on('update player position', function(player_data){
     var cleaned_player = _.extend({type: 'Other Player'}, player_data);
     var old_player_data = map.get_player(player_data.username);
-    var updated_map = map.update_local_map(socket, old_player_data, player_data);
-    var current_room = socket_helpers.get_socket_room_name(cleaned_player);
+    var updated_map = current_user.update_map(old_player_data, player_data);
+    var current_room = socket_helpers.get_room_name(cleaned_player);
     socket.emit('update map', updated_map);
     map.update_player(cleaned_player);
     socket.broadcast.to(current_room).emit('update player position', cleaned_player);
@@ -27,7 +30,7 @@ io.on('connection', function(socket){
     var uniqueID = Math.random().toString(); 
     var new_player = {username: player_data.username, coordinate: {x: 130, y: 130, z: 1}, uniqueID: uniqueID};
     var cleaned_player = _.extend({type: 'Other Player'}, new_player);
-    var current_room = socket_helpers.get_socket_room_name(cleaned_player);
+    var current_room = socket_helpers.get_room_name(cleaned_player);
     map.add_player(cleaned_player);
     socket.emit('player request', new_player);
     socket.broadcast.to(current_room).emit('add other player', cleaned_player);
