7db0249704ac4bf4f2dbec4a30c9d9ef239ccc76
Zach Serafini <serafini.8@osu.edu>
2014-11-02 18:55:34 -0500

    Start some specs

--- spec/server/map-spec.js
+++ spec/server/map-spec.js
@@ -1,4 +1,5 @@
 var map = require("../../lib/map.js");
+var valid_map_object = jasmine.objectContaining({type: jasmine.any(String), uniqueID: jasmine.any(String), coordinate: jasmine.objectContaining({ x: jasmine.any(Number), y: jasmine.any(Number)})});
 
 describe("map", function() {
   it("runs the specs", function() {
@@ -6,5 +7,20 @@ describe("map", function() {
   });
   it("can access map functions", function() {
     expect(map.radius).toBe(8);
+  });
+});
+
+describe("Initializing the map", function() {
+
+  beforeEach(function() {
+    map.initialize();
+  });
+
+  it("populates the map layout with objects", function() {
+    expect(map.layout[0][0][0]).toEqual(valid_map_object);
+    expect(map.layout[0][199][0]).toEqual(valid_map_object);
+    expect(map.layout[199][0][0]).toEqual(valid_map_object);
+    expect(map.layout[199][199][0]).toEqual(valid_map_object);
+    expect(map.layout[100][100][0]).toEqual(valid_map_object);
   });
 });
